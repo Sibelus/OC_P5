01d91c43bbaadb6bc481cc0462e20247460bc62f
Gaëtan <ggaetan@proton.me>
2022-12-18 08:21:48 +0100

    Update AlertController test

--- src/test/java/com/safetynet/api/controller/AlertControllerTest.java
+++ src/test/java/com/safetynet/api/controller/AlertControllerTest.java
@@ -1,5 +1,6 @@
 package com.safetynet.api.controller;
 
+import com.safetynet.api.service.ChildAlertService;
 import com.safetynet.api.service.FirestationAlertService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +11,26 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = FirestationAlertController.class)
-public class FirestationAlertControllerTest {
+@WebMvcTest
+public class AlertControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private FirestationAlertService firestationAlertService;
+    @MockBean
+    private ChildAlertService childAlertService;
 
     @Test
     public void testGetFirestationAlert() throws Exception {
         mockMvc.perform(get("/FirestationAlert/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetChildAlert() throws Exception{
+        mockMvc.perform(get("/ChildAlert/1509 Culver St"))
                 .andExpect(status().isOk());
     }
 }
