afe6959a3e2a7f8366900f12ad01de4cf24da7b2
4H0 <ggaetan@proton.me>
2022-12-29 10:23:33 +0100

    Update PersonRepository & PersonRepositoryIT

--- src/main/java/com/safetynet/api/repository/PersonRepository.java
+++ src/main/java/com/safetynet/api/repository/PersonRepository.java
@@ -9,49 +9,83 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-
-
 @Repository
 public class PersonRepository {
 
     public ArrayList<Person> getPersonsAggregatedData() {
-
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Persons persons;
         //ArrayList that aggregate all data
         ArrayList<Person> personsAggregatedData = new ArrayList<>();
 
-        {
-            try {
-                persons = objectMapper.readValue(new File("src/main/resources/data.json"), Persons.class);
-                Firestations firestations = objectMapper.readValue(new File("src/main/resources/data.json"), Firestations.class);
-                Medicalrecords medicalrecords = objectMapper.readValue(new File("src/main/resources/data.json"), Medicalrecords.class);
+        ArrayList<Person> allPersons = getPersonsData();
+        ArrayList<Firestation> allFirestations = getFirestationsData();
+        ArrayList<Medicalrecord> allMedicalrecords = getMedicalrecordsData();
 
-                for(Person citizen : persons.getPersons()) {
-                    //add firestation number
-                    for(Firestation firestation : firestations.getFirestations()){
-                        if (citizen.getAddress().equals(firestation.getAddress())) {
-                            citizen.setStation(firestation.getStation());
-                        }
-                    }
 
-                    //add medicalrecords
-                    for(Medicalrecord medicalrecord : medicalrecords.getMedicalrecords()){
-                        if(medicalrecord.getFirstName().equals(citizen.getFirstName()) && medicalrecord.getLastName().equals(citizen.getLastName())){
-                            citizen.setBirthdate(medicalrecord.getBirthdate());
-                            citizen.setMedications(medicalrecord.getMedications());
-                            citizen.setAllergies(medicalrecord.getAllergies());
-                        }
-                    }
-                    personsAggregatedData.add(citizen);
+        for(Person person : allPersons) {
+            //add firestation number
+            for(Firestation firestation : allFirestations){
+                if (person.getAddress().equals(firestation.getAddress())) {
+                    person.setStation(firestation.getStation());
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
+            //add medicalrecords
+            for(Medicalrecord medicalrecord : allMedicalrecords){
+                if(medicalrecord.getFirstName().equals(person.getFirstName()) && medicalrecord.getLastName().equals(person.getLastName())){
+                    person.setBirthdate(medicalrecord.getBirthdate());
+                    person.setMedications(medicalrecord.getMedications());
+                    person.setAllergies(medicalrecord.getAllergies());
+                }
+            }
+            personsAggregatedData.add(person);
         }
-
         return personsAggregatedData;
     }
 
+
+    public ArrayList<Person> getPersonsData() {
+        ArrayList<Person> allPersons = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Persons persons = null;
+
+        try {
+            persons = objectMapper.readValue(new File("src/main/resources/data.json"), Persons.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        allPersons.addAll(persons.getPersons());
+
+        return allPersons;
+    }
+
+
+    public ArrayList<Firestation> getFirestationsData() {
+        ArrayList<Firestation> allFirestations = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Firestations firestations = null;
+
+        try {
+            firestations = objectMapper.readValue(new File("src/main/resources/data.json"), Firestations.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        allFirestations.addAll(firestations.getFirestations());
+
+        return allFirestations;
+    }
+
+
+    public ArrayList<Medicalrecord> getMedicalrecordsData() {
+        ArrayList<Medicalrecord> allMedicalrecords = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Medicalrecords medicalrecords = null;
+
+        try {
+            medicalrecords = objectMapper.readValue(new File("src/main/resources/data.json"), Medicalrecords.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        allMedicalrecords.addAll(medicalrecords.getMedicalrecords());
+
+        return allMedicalrecords;
+    }
 }
---
+++ src/test/java/com/safetynet/api/repository/PersonRepositoryIT.java
@@ -0,0 +1,44 @@
+package com.safetynet.api.repository;
+
+import com.safetynet.api.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.ArrayList;
+
+@SpringBootTest
+public class PersonRepositoryIT {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void testAggregateData() {
+        ArrayList<Person> allPersons = personRepository.getPersonsAggregatedData();
+
+        Assertions.assertEquals(3 , allPersons.get(0).getStation());
+        Assertions.assertEquals("03/06/1984", allPersons.get(0).getBirthdate());
+    }
+
+    @Test
+    public void testGetPersonData(){
+        ArrayList<Person> allPersons = personRepository.getPersonsData();
+
+        Assertions.assertEquals("John", allPersons.get(0).getFirstName());
+    }
+
+    @Test
+    public void testGetFirestationData(){
+        ArrayList<Firestation> allFirestations = personRepository.getFirestationsData();
+
+        Assertions.assertEquals("1509 Culver St", allFirestations.get(0).getAddress());
+    }
+
+    @Test
+    public void testGetMedicalrecordData(){
+        ArrayList<Medicalrecord> allMedicalrecords = personRepository.getMedicalrecordsData();
+
+        Assertions.assertEquals("03/06/1984", allMedicalrecords.get(0).getBirthdate());
+    }
+}
